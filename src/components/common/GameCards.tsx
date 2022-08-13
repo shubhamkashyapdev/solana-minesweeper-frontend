8e7c7d825efefe5f71a0ea3a2b35f145099b0f7e
Shubham Kashyap <shubhamkashyapdev@gmail.com>
2022-08-13 21:35:37 +0530

    link added on home page

--- src/components/common/GameCards.tsx
+++ src/components/common/GameCards.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const GameCards = () => {
   return (
     <div className="flex justify-center space-y-4 md:justify-between items-center flex-wrap ">
-      <img
-        className="h-[400px] md:h-[450px]"
-        src="/assets/images/cards/minesweeper.png"
-      />
+      <Link to="/minesweeper">
+        <img
+          className="h-[400px] md:h-[450px]"
+          src="/assets/images/cards/minesweeper.png"
+        />
+      </Link>
       <img
         className="h-[400px] md:h-[450px]"
         src="/assets/images/cards/under-construction.png"
