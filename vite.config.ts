5a44aa0b3c4086974b7b73816a2aa44fa472ca86
Shubham Kashyap <shubhamkashyapdev@gmail.com>
2022-08-26 20:24:52 +0530

    polyfil added for Buffer

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,15 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
 import { fileURLToPath } from "url"
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 const __filename = fileURLToPath(import.meta.url)
 
 const __dirname = path.dirname(__filename)
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), NodeGlobalsPolyfillPlugin({
+    buffer: true
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
