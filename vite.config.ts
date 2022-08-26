3ba76c5f1e6f07bce9cd96d7257c3014ae26d4e5
Shubham Kashyap <shubhamkashyapdev@gmail.com>
2022-08-26 20:42:54 +0530

    Node Plyfilled using rollup

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,13 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
 import { fileURLToPath } from "url"
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import polyfillNode from 'rollup-plugin-polyfill-node'
 const __filename = fileURLToPath(import.meta.url)
 
 const __dirname = path.dirname(__filename)
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), NodeGlobalsPolyfillPlugin({
-    buffer: true
-  })],
+  plugins: [react(), polyfillNode()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
