b3b4f6044c0d98cbccdefebd00f8c8fbee7f8899
Shubham Kashyap <shubhamkashyapdev@gmail.com>
2022-08-26 20:49:10 +0530

    Buffer configured manually

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { BrowserRouter, BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 import App from "./App"
 import "./index.css"
 import { NotificationsProvider } from '@mantine/notifications';
 import { MantineProvider } from '@mantine/core'
+import { Buffer } from 'buffer'
+window.Buffer = Buffer
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Router>
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
 import { fileURLToPath } from "url"
-import polyfillNode from 'rollup-plugin-polyfill-node'
 const __filename = fileURLToPath(import.meta.url)
 
 const __dirname = path.dirname(__filename)
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), polyfillNode()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
